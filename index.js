cd0613cdf96a654a7aaa02e7bf230ad6279a3209
Luke Oliff <luke@lukeoliff.com>
2020-10-07 15:45:35 +0100

    rejig build script for table output

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-const fs = require('fs');
-const { convertFile } = require('convert-svg-to-png');
+const fs = require('fs')
+const { convertFile } = require('convert-svg-to-png')
 
 const config = require('./.heartrc.json')
 
-const srcDir = fs.readdirSync('./src');
-const srcFiles = srcDir.filter(e => e.match(/.*\.svg/ig));
+const srcDir = fs.readdirSync('./src')
+const srcFiles = srcDir.filter(e => e.match(/.*\.svg/ig))
 
-let destFilename;
-
-process.setMaxListeners(0);
+process.setMaxListeners(0)
 
 const convertHearts = (srcDir, buildDir) => {
   if (!fs.existsSync(srcDir)) {
-    fs.mkdirSync(srcDir);
+    fs.mkdirSync(srcDir)
   }
   if (!fs.existsSync(buildDir)) {
-    fs.mkdirSync(buildDir);
+    fs.mkdirSync(buildDir)
   }
-  config.sizes.map((size) => {
-    srcFiles.map((file) => {
-      destFilename = file.substring(0, file.length - 4) + '_' + size + '.png';
-      convertFile(srcDir + file, {
-        outputFilePath: buildDir + destFilename,
-        width: size,
-        height: size
+
+  const fileTable = srcFiles.map(file => {
+    const name = file.substring(0, file.length - 4)
+
+    return {
+      name: `:${name}:`,
+      path: file,
+      pngs: config.sizes.map(
+        size => ({ 
+          size,
+          name: `${name}${size/config.defaultSize === 1 ? '' : `@${size/config.defaultSize}x`}.png`
+        })
+      )
+    }
+  })
+
+  console.log(fileTable)
+
+  fileTable.forEach(file => {
+    file.pngs.forEach(png => {
+      convertFile(srcDir + file.path, {
+        outputFilePath: buildDir + png.name,
+        width: png.size,
+        height: png.size
       })
         .then(d => console.log('Created ' + d))
-        .catch(e => console.error(e));
-    });
-  });
+        .catch(e => console.error(e))
+    })
+  })
 }
 
-convertHearts(config.srcDir, config.destDir);
+convertHearts(config.srcDir, config.destDir)
